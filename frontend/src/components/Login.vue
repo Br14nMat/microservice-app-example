936518a9c181bd5c56439f7c7f10a216d50a2b88
Br14nMat <1108638502@u.icesi.edu.co>
2025-04-28 10:07:15 -0500

    change color

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -7,7 +7,7 @@
                 <div class='row'>
                     <div class='col-md-3'></div>
                     <div class='col-md-6'>
-                        <h2>Cambio en vivo (boton amarillo -> rojo)</h2>
+                        <h2>Cambio en vivo (boton verde -> rojo)</h2>
                         <hr>
                     </div>
                 </div>
